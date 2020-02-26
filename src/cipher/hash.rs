241c5e4f19270f2a0fb133b4e1954026bbd22cb6
ivan770 <leshenko.ivan770@gmail.com>
2020-02-26 20:50:00 +0200

    Add hasher tests

--- src/cipher/hash.rs
+++ src/cipher/hash.rs
@@ -17,3 +17,18 @@ impl Hash {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use hex_literal::hex;
+    use super::Hash;
+
+    #[test]
+    fn is_hashing_correctly()
+    {
+        let hash = Hash::make("test");
+
+        assert_eq!(hash.encrypt, hex!("8288166e7fbe796883a4f2e9b3ca9f484f521d0ce464345cc1aec96779149c14"));
+        assert_eq!(hash.mac, hex!("9ece086e9bac491fac5c1d1046ca11d737b92a2b2ebd93f005d7b710110c0a67"));
+    }
+}
