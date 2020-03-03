86d2a22f6ca329100c317d7a05311e641b1bfc15
ivan770 <leshenko.ivan770@gmail.com>
2020-03-03 15:11:14 +0200

    Add empty string hash test

--- src/lib/hashers/sha3_512.rs
+++ src/lib/hashers/sha3_512.rs
@@ -34,4 +34,18 @@ mod tests {
             hex!("9ece086e9bac491fac5c1d1046ca11d737b92a2b2ebd93f005d7b710110c0a67")
         );
     }
+
+    #[test]
+    fn is_hashing_empty_string_correctly() {
+        let hash = Sha3_512::make("");
+
+        assert_eq!(
+            hash.encrypt,
+            hex!("15B2123AF1F5F94C11E3E9402C3AC558F500199D95B6D3E301758586281DCD26")
+        );
+        assert_eq!(
+            hash.mac,
+            hex!("A69F73CCA23A9AC5C8B567DC185A756E97C982164FE25859E0D1DCC1475C80A6")
+        );
+    }
 }
